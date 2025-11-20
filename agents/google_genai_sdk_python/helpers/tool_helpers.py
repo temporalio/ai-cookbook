e83ca600edc7749db4d2493cddcfb032a77ee7c0
Rick Ross <rick.ross@temporal.io>
2025-11-20 15:52:43 -0500

    Removed old comment

--- agents/google_genai_sdk_python/helpers/tool_helpers.py
+++ agents/google_genai_sdk_python/helpers/tool_helpers.py
@@ -5,12 +5,3 @@ You have access to various tools and should use them when they can help answer t
 Always prefer using tools when they are available and relevant to the user's query.
 If no tools are needed to answer the question, respond in haikus.
 """
-
-#### Original Prompt doesn't infer location from an IP address
-#
-# HELPFUL_AGENT_SYSTEM_INSTRUCTIONS = """
-# You are a helpful agent that can use tools to help the user.
-# You will be given a input from the user and a list of tools to use.
-# You may or may not need to use the tools to satisfy the user ask.
-# If no tools are needed, respond in haikus.
-# """
